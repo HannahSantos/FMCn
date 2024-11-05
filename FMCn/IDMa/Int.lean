5f43a8d23b3f73a4728914e8a2a462d783921175
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-11-05 13:02:24 -0300

    Added ZA_InvL theorem

--- FMCn/IDMa/Int.lean
+++ FMCn/IDMa/Int.lean
@@ -4,4 +4,10 @@ theorem ZA_IdL :
   ∀ (a : Int), 0 + a = a :=
 by
   intro a
-  rw [ZA_Com, ZA_IdR]
+  rw [ZA_Com 0 a, ZA_IdR a]
+
+theorem ZA_InvL :
+  ∀ (a : Int), -a + a = 0 :=
+by
+  intro a
+  rw [ZA_Com (-a) a, ZA_InvR a]
