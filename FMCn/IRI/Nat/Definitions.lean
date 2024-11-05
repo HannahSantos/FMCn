eb93d92d3ec1b9fa8004857dad67e895069929b3
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-11-05 12:57:53 -0300

    Added new Nat functions

--- FMCn/IRI/Nat/Definitions.lean
+++ FMCn/IRI/Nat/Definitions.lean
@@ -1,9 +1,16 @@
+import FMCn.CFR1.Functions
 namespace data
 
 inductive Nat where
   | O : Nat
   | S : Nat → Nat
 deriving Repr
+
+def eq : Nat → Nat → Bool
+  | .O, .O => .true
+  | .S n, .S m => eq n m
+  | _, _ => .false
+infix:65 " == " => eq
 
 def pred : Nat → Nat
   | .O => .O
@@ -29,5 +36,41 @@ def leq : Nat → Nat → Bool
   | _, .O => false
   | .S n, .S m => leq n m
 infix:65 " ≤ " => leq
+
+def lt : Nat → Nat → Bool
+  | _, .O => false
+  | .O, _ => true
+  | .S n, .S m => lt n m
+infix:65 " < " => lt
+
+def double : Nat → Nat
+  := uncurry add ∘ Δ
+
+def min₂ : Nat → Nat → Nat
+  | .S n, .S m => .S (min₂ n m)
+  | _, _ => .O
+
+def max₂ : Nat → Nat → Nat
+  | .O, m => m
+  | n, .O => n
+  | .S n, .S m => .S (max₂ n m)
+
+mutual
+def even : Nat → Bool
+  | .O => true
+  | .S n => odd n
+
+def odd : Nat → Bool
+  | .O => false
+  | .S n => even n
+end
+
+def Zero : Nat → Bool
+  | .O => true
+  | _ => false
+
+def stripMaybe : Nat ⊕ Unit → Nat
+  | .inl n => n
+  | .inr () => .O
 
 axiom peano_neg {n : Nat} : ¬ Nat.O = Nat.S n
