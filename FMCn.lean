e4a49db3e7e3d74ac092c244f57af5fce7c11e09
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-11-06 14:26:29 -0300

    Updated imports

--- FMCn.lean
+++ FMCn.lean
@@ -7,6 +7,9 @@ import «FMCn».CFR1.Useful
 import «FMCn».IEA.Definitions
 import «FMCn».IEA.Monoid
 import «FMCn».IEA.Group
+import «FMCn».IEA.«Group-Homo»
 import «FMCn».IRI.Nat.Definitions
 import «FMCn».IRI.Nat.Theorems
+import «FMCn».IRI.List.Definitions
+import «FMCn».IRI.List.Theorems
 import «FMCn».Intro_Lang_Proofs
