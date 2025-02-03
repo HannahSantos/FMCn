d2a3adc76aa74b0fc73c528e83b5619edd9633c3
HannahSantos <hannah.santos.441@ufrn.edu.br>
2025-02-03 19:46:13 -0300

    Added map and depth

--- FMCn/IRI/Trees/BinTree/Definitions.lean
+++ FMCn/IRI/Trees/BinTree/Definitions.lean
@@ -6,6 +6,10 @@ inductive BinTree (α : Type) where
   | Leaf : α → BinTree α
   | Fork : BinTree α → BinTree α → BinTree α
 
+def BTmap : (α → β) → BinTree α → BinTree β
+  | f, .Leaf x => .Leaf (f x)
+  | f, .Fork l r => .Fork (BTmap f l) (BTmap f r)
+
 def nodes : BinTree α → Nat
   | .Leaf _ => .O
   | .Fork l r => .S (nodes l + nodes r)
@@ -13,4 +17,8 @@ def nodes : BinTree α → Nat
 def tips : BinTree α → Nat
   | .Leaf _ => .S .O
   | .Fork l r => tips l + tips r
+
+def depth : BinTree α → Nat
+  | .Leaf _ => .O
+  | .Fork l r => .S (max₂ (depth l) (depth r))
 
